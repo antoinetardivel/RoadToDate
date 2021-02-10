17cccade5c34315da9640c6dfeb8211826e1e1a3
Hugo Blanchard <hugo.blanchard@mmibordeaux.com>
2021-02-10 14:06:48 +0100

    scores layout

---
+++ src/components/Settingsbutton.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Settingsbutton = () => {
+  return <View style={styles.settingsbox}></View>;
+};
+
+const styles = StyleSheet.create({
+  settingsbox: {
+    position: "absolute",
+    right: 20,
+    top: 20,
+    height: 55,
+    width: 55,
+    borderRadius: 14,
+    borderStyle: "solid",
+    borderColor: "#682B16",
+    borderWidth: 2,
+    backgroundColor: "#8F2F30",
+  },
+});
+
+export default Settingsbutton;
