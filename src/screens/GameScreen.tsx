c01b6da917ddb16efcf2e1524dcf897f5ee711ac
antoinetardivel <tardivelantoine@gmail.com>
2021-02-10 11:23:59 +0100

    déplacement joueur op

--- src/screens/GameScreen.tsx
+++ src/screens/GameScreen.tsx
@@ -3,7 +3,6 @@ import { StyleSheet, Text, View, Dimensions  } from 'react-native';
 import { Accelerometer } from 'expo-sensors';
 
 export default function GameScreen() {
-
   const [data, setData] = useState({
       x: 0,
       y: 0,
@@ -18,20 +17,29 @@ export default function GameScreen() {
 
   let windowWidth = Dimensions.get('window').width
   let windowHeight = Dimensions.get('window').height
-
+  const [PosPlayerY, setPosPlayerY] = useState((windowWidth/2)-25)
   console.log(Math.round(y))
-  const PosPerso = React.useMemo(() => {
-    return Math.round(y);
-  }, [y])
-
+  let PosTemp = 0
+  useEffect(()=>{
+    PosTemp = (windowWidth/2) + ((windowWidth*( parseFloat(y).toFixed( 2 )) )-25)
+    if (PosTemp < 0){
+      PosTemp = 0
+    }else if (PosTemp > (windowWidth - 50)){
+      PosTemp = (windowWidth-50)
+    }else{
+      PosTemp = (windowWidth/2) + ((windowWidth*( parseFloat(y).toFixed( 2 )) )-25)
+    }
+    setPosPlayerY(PosTemp)
+  },[y])
   
+
 
   return (
   <View style={styles.Container}>
-      <View style={styles.Perso}></View>
-      <Text>{Math.round(y)} </Text>
-      <Text>-- {windowWidth} </Text>
-      <Text>-- {windowHeight} </Text>
+    <View style={[styles.Perso, {left: PosPlayerY}]}></View>
+    <Text>{ parseFloat(y).toFixed( 2 )} </Text>
+    <Text>-- {windowWidth} </Text>
+    <Text>-- {windowHeight} </Text>
   </View>
   );
 }
