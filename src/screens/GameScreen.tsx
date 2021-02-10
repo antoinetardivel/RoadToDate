26a3ab9c8f7119d76ab07e7ee660767afb9f669d
antoinetardivel <tardivelantoine@gmail.com>
2021-02-10 10:50:28 +0100

    détecttion taille page

--- src/screens/GameScreen.tsx
+++ src/screens/GameScreen.tsx
@@ -1,27 +1,52 @@
-import React, {useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, Dimensions  } from 'react-native';
 import { Accelerometer } from 'expo-sensors';
 
 export default function GameScreen() {
 
-    const [data, setData] = useState({
-        x: 0,
-        y: 0,
-        z: 0,
-        });
+  const [data, setData] = useState({
+      x: 0,
+      y: 0,
+      z: 0,
+      });
 
-    Accelerometer.setUpdateInterval(100);
-    Accelerometer.addListener(accelerometerData => {
-        setData(accelerometerData);
-      })
+  Accelerometer.setUpdateInterval(100);
+  Accelerometer.addListener(accelerometerData => {
+      setData(accelerometerData);
+    })
+  const { x, y, z } = data;
 
-    const { x, y, z } = data;
+  let windowWidth = Dimensions.get('window').width
+  let windowHeight = Dimensions.get('window').height
 
-    console.log(Math.round(y))
+  console.log(Math.round(y))
+  const PosPerso = React.useMemo(() => {
+    return Math.round(y);
+  }, [y])
 
-    return (
-    <View>
-        <Text>{y} </Text>
-    </View>
-    );
+  
+
+  return (
+  <View style={styles.Container}>
+      <View style={styles.Perso}></View>
+      <Text>{Math.round(y)} </Text>
+      <Text>-- {windowWidth} </Text>
+      <Text>-- {windowHeight} </Text>
+  </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  Container: {
+    backgroundColor: 'green',
+    flex: 1,
+    height:'100%',
+  },
+  Perso: {
+      position: 'absolute',
+      width: 50,
+      height: 50,
+      backgroundColor: 'red',
+      bottom: 30,
+  }
+})
